8a2549347a63c3dcf3f52f8c5307152597086e60
amirmohammadkhezri <49806187+amirmohammadkhezri@users.noreply.github.com>
2020-01-24 00:06:50 +0330

    add folder containers/App/*.js

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,12 +7,19 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+
+import Button from 'components/Button';
+
 import messages from './messages';
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <div>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
+
+      <Button>hello sayad</Button>
+    </div>
   );
 }
